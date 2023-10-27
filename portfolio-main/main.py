5ff87d715ee5934f04b787937e48976ab9078870
Jproga <138982927+Jproga@users.noreply.github.com>
2023-10-27 12:03:19 +0300

    Update main.py

--- portfolio-main/main.py
+++ portfolio-main/main.py
@@ -9,13 +9,16 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
+ 
 #Динамичные скиллы
 @app.route('/', methods=['POST'])
 def process_form():
     button_python = request.form.get('button_python')
-    return render_template('index.html', button_python=button_python)
+    button_discord = request.form.get('button_discord')
+    button_db = request.form.get('button_db')
+    return render_template('index.html', button_python=button_python, 
+                           button_discord=button_discord,
+                           button_db=button_db)
 
 
 if __name__ == "__main__":
